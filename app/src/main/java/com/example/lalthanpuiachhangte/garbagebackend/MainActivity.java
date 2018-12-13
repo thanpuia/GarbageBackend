8a8b518290f209ba9f526adcb877abd27de81060
Tpa Chhangte <thanpuia46@gmail.com>
2018-12-13 12:41:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lalthanpuiachhangte/garbagebackend/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.lalthanpuiachhangte.garbagebackend;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    public DatabaseReference databaseReference;
+    public LocationManager locationManager;
+    public LocationListener locationListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //Location lastKnowLocation = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                databaseReference = FirebaseDatabase.getInstance().getReference();
+                String newKey = databaseReference.child("truck-2/location").push().getKey();
+
+                databaseReference.child("truck-2/location/"+ newKey).child("latitude").setValue(location.getLatitude());
+                databaseReference.child("truck-2/location/"+ newKey).child("longitude").setValue(location.getLongitude());
+            }
+            @Override public void onStatusChanged(String provider, int status, Bundle extras) { }
+            @Override public void onProviderEnabled(String provider) { }
+            @Override public void onProviderDisabled(String provider) { }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,800,5,locationListener);
+    }
+}
